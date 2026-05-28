1c7411c4123511546d363eda9130b2355180ed30
rianhussain007 <786rianhussain@gmail.com>
2026-05-28 15:55:06 +0530

    Update docs from fresh out build

--- next.config.mjs
+++ next.config.mjs
@@ -3,19 +3,16 @@ import { dirname } from 'path';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-const basePath = process.env.NEXT_PUBLIC_BASE_PATH || "";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
   trailingSlash: true,
   basePath: '/HAI7',
-  assetPrefix: basePath,
   images: {
     unoptimized: true,
   },
   webpack(config) {
-    // Grab the existing rule that handles SVG imports
     const fileLoaderRule = config.module.rules.find((rule) =>
       rule.test?.test?.(".svg")
     );
